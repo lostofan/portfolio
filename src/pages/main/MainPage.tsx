5fbe2579c352b6cc7d955ad5b0f299a9d9759f79
lostofan <kolog007@gmail.com>
2023-08-18 13:28:46 +0300

    import fix

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Home } from '../../components/sections/Greeting/Greeting';
+import { Greeting } from '../../components/sections/Greeting/Greeting';
 import { Projects } from '../../components/sections/Projects/Projects';
 import { Skills } from '../../components/sections/Skills/Skills';
 import { About } from '../../components/sections/About/About';
@@ -12,7 +12,7 @@ export const MainPage = () => {
     <main>
       <div className="main__wrapper">
         <Header />
-        <Home />
+        <Greeting />
         <Projects isPage={false} />
         <Skills isPage={false} />
         <About isPage={false} />
