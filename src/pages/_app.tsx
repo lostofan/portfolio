df96c50d00bbde8655e16cfcf93b974ac5e7c6ca
lostofan <kolog007@gmail.com>
2023-08-18 14:29:16 +0300

    finally font works

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import '../styles/reset.scss';
 import '../styles/index.scss';
+import '../styles/fonts.scss';
 import '../styles/animations.scss';
+import '../styles/variables.scss';
 
 import 'aos/dist/aos.css';
 
