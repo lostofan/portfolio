9d8fc1799432a639eff1b6faa25353c7bbc8d8e9
lostofan <kolog007@gmail.com>
2023-08-18 22:54:52 +0300

    lang select in burger

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './BurgerMenu.module.scss';
 import { LinksBar } from 'components/LinksBar/LinksBar';
+import Link from 'next/link';
+import router, { useRouter } from 'next/router';
 
 interface BurgerMenuProps {
   names: string[];
@@ -8,17 +10,41 @@ interface BurgerMenuProps {
 }
 
 export const BurgerMenu: React.FC<BurgerMenuProps> = ({ names, menuActive }) => {
+  const router = useRouter();
   return (
     <nav className={!menuActive ? styles.root : `${styles.root} ${styles.opened}`}>
       <ul className={styles.list}>
-        {names.map((name) => (
-          <li className={styles.item} key={name}>
+        {names.map((name, idx) => (
+          <li className={styles.item} key={idx}>
             <a href={`#${name}`} className={styles.link}>
               <span className="tag">#</span>
               {name}
             </a>
           </li>
         ))}
+        <li className={styles.langs}>
+          <Link
+            href="/"
+            locale="en"
+            className={
+              router.locale === 'en'
+                ? `${styles.languageBtn} ${styles.languageBtn_active}`
+                : styles.languageBtn
+            }>
+            En
+          </Link>
+          /
+          <Link
+            href="/"
+            locale="ru"
+            className={
+              router.locale === 'ru'
+                ? `${styles.languageBtn} ${styles.languageBtn_active}`
+                : styles.languageBtn
+            }>
+            Ru
+          </Link>
+        </li>
       </ul>
       <LinksBar />
     </nav>
